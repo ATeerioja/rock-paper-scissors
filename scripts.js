44beb02126b3b5620d8e8cb213759c448d1028ea
ATeerioja <teerioja.opiskelu@gmail.com>
2024-07-17 11:28:10 +0300

    Display events works

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-
 function getComputerChoice() {
   //Get a number between 0 and 1
   let randomNum = Math.random();
@@ -15,12 +14,15 @@ function getComputerChoice() {
 
 }
 
+
+
 function playGame() {
   
   let humanScore = 0
   let computerScore = 0
+  
+  
   const computerSelection = getComputerChoice();
-
   const rock = document.querySelector("#rock");
   rock.addEventListener("click", () => playRound("rock", computerSelection));
 
@@ -30,30 +32,35 @@ function playGame() {
   const scissors = document.querySelector("#scissors");
   scissors.addEventListener("click", () => playRound("scissors", computerSelection));
   
-
   function playRound(humanChoice, computerChoice) {
     //First check is for draws
     if(humanChoice === computerChoice) {
-      console.log(`Draw ${humanChoice} is the same as ${computerChoice}`);
+      displayEvent(`Draw ${humanChoice} is the same as ${computerChoice}`);
   
+    //Second check is for the player to win
     } else if(humanChoice === "rock" && computerChoice === "scissors"
     || humanChoice === "paper" && computerChoice === "rock"
     || humanChoice === "scissors" && computerChoice === "paper"){
-      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+      displayEvent(`You win! ${humanChoice} beats ${computerChoice}`);
       humanScore ++;
   
+    //if the player didn't win, it has to lose
     } else {
-      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+      displayEvent(`You lose! ${computerChoice} beats ${humanChoice}`);
       computerScore ++;
   
     }
   }
 
-  if(humanScore > computerScore) {
-    console.log("You Win the Game !!");
-  } else {
-    console.log("You Lose the Game !!");
+  function displayEvent(text) {
+    const box = document.querySelector(".event-banner");
+    const result = document.createElement("p");
+    result.textContent = text;
+
+    box.appendChild(result);
+
   }
+
 
 }
 
